4fce5fc8b1f631256618998962147627aa10ced5
Aman Chauhan <aman.chauhan@wittybrains.com>
2026-07-23 17:02:27 +0530

    json structure

--- app/llm/clients.py
+++ app/llm/clients.py
@@ -28,7 +28,10 @@ class   LLMClient:
                     "role": "user",
                     "content": user_prompt
                 }
-            ]
+            ],
+            response_format={
+                "type": "json_object"
+            }
         )
 
         return response.choices[0].message.content
